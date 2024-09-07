2da6b2aa6523e09525d4246d03169854fc341a68
Bas3L3ss <phamthen.hun060907@gmail.com>
2024-09-07 15:19:37 +0700

    commit

--- src/app/api/webhooks/clerk.ts
+++ src/app/api/webhooks/clerk.ts
@@ -1,48 +1,36 @@
-// pages/api/webhooks/clerk.ts
+import { NextResponse } from "next/server";
+import { WebhookEvent } from "@clerk/nextjs/server";
+import prisma from "@/utils/db";
 
-import type { NextApiRequest, NextApiResponse } from "next";
-import { PrismaClient } from "@prisma/client";
-import { verifySignature } from "@clerk/nextjs/server";
-
-const prisma = new PrismaClient();
-
-const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  // Verify the Clerk webhook signature
-  const signature = req.headers["clerk-signature"] as string;
-  const payload = req.body;
-
-  if (!verifySignature(signature, payload)) {
-    return res.status(400).json({ message: "Invalid signature" });
-  }
-
+export async function POST(request: Request) {
   try {
-    // Handle the webhook event
-    if (req.method === "POST") {
-      const event = req.body;
+    const payload: WebhookEvent = await request.json();
 
-      if (event.type === "user.created") {
-        const userData = event.data;
-        await prisma.user.create({
-          data: {
-            clerkId: userData.id,
-            userName: userData.username || "",
-            userBiography: userData.biography || "",
-            userAvatar: userData.avatar || "",
-            // Set other fields as needed
-          },
-        });
-        res.status(200).json({ message: "User created in database" });
-      } else {
-        res.status(200).json({ message: "Event type not handled" });
-      }
-    } else {
-      res.setHeader("Allow", ["POST"]);
-      res.status(405).end(`Method ${req.method} Not Allowed`);
+    console.log(payload);
+
+    if (payload.type === "user.created") {
+      const userData = payload.data;
+
+      // Create the user in your database
+      await prisma.user.create({
+        data: {
+          clerkId: userData.id,
+          userName: userData.username || "",
+          userBiography: "",
+          userAvatar: userData.image_url || "",
+          // Set other fields as needed
+        },
+      });
+
+      return NextResponse.json({ message: "User created in database" });
     }
+
+    return NextResponse.json({ message: "Event type not handled" });
   } catch (error) {
     console.error("Error handling webhook", error);
-    res.status(500).json({ message: "Internal Server Error" });
+    return NextResponse.json(
+      { message: "Internal Server Error" },
+      { status: 500 }
+    );
   }
-};
-
-export default handler;
+}
