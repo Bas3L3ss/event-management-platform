79c9f2666fc75d90b0d501f541afd13af0d6069a
Bas3L3ss <phamthen.hun060907@gmail.com>
2024-09-07 15:02:08 +0700

    commit

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,6 +1,9 @@
 import { SignIn } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 import React from "react";
 
 export default function Page() {
+  if (auth().userId) redirect("/");
   return <SignIn />;
 }
--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,9 @@
 import { SignUp } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
 export default function Page() {
+  if (auth().userId) redirect("/");
+
   return <SignUp />;
 }
