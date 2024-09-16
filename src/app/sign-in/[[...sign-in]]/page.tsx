c659bc49365183ce76462d4cb2d0f7f226d7b1dc
Bas3L3ss <phamthen.hun060907@gmail.com>
2024-09-16 17:35:59 +0700

    commti

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,8 +1,6 @@
 import { SignIn } from "@clerk/nextjs";
-import { auth } from "@clerk/nextjs/server";
-import { redirect } from "next/navigation";
 import React from "react";
 
-export default function Page() {
+export default function SignInPage() {
   return <SignIn />;
 }
--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -1,7 +1,5 @@
 import { SignUp } from "@clerk/nextjs";
-import { auth } from "@clerk/nextjs/server";
-import { redirect } from "next/navigation";
 
-export default function Page() {
+export default function SignUpPage() {
   return <SignUp />;
 }
