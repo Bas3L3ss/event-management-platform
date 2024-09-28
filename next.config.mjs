0384bab0d512031799b5dada1d25fee9f0a42afe
Bas3L3ss <phamthen.hun060907@gmail.com>
2024-09-29 00:24:36 +0700

    commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "27mb",
+    },
+  },
   productionBrowserSourceMaps: true,
   images: {
     remotePatterns: [
