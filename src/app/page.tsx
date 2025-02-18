6444520edc4bb7d379061672365d37da90bd2b34
Bas3L3ss <phamthen.hun060907@gmail.com>
2025-02-18 14:28:28 +0700

    feat(landing-page): improve performace

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,12 @@
-import { Suspense } from "react";
 import FeaturedEventsPage from "@/components/FeaturedEvents";
 import Hero from "@/components/Hero";
 import RecommendationCarousel from "@/components/RecomendationCarousel";
-import SkeletonLoading from "@/components/SkeletonLoading";
-import { LoadingVariant } from "@/constants/values";
 
 function Home() {
   return (
     <>
       <Hero />
-
       <FeaturedEventsPage />
-
       <RecommendationCarousel />
     </>
   );
